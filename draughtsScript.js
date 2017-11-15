860d3876384b476b12d0b0513baf9b3a5906c35a
Mark Fitzpatrick <markfitz815@gmail.com>
2017-11-15 14:27:17 +0000

    boardArr given all necessary values

--- draughtsScript.js
+++ draughtsScript.js
@@ -5,20 +5,24 @@ let boardArr = [];
 function createBoardArr(){
 
   for(var i = tileAmt; i >= 1; i--){
-
-    for(var j = 0; j < tileAmt; j++){
-
+    for(var j = 1; j <= tileAmt; j++){
       const tmpObj = new Object;
       tmpObj.empty = true;
       tmpObj.x = i;
-      tmpObj.y = String.fromCharCode(65+j);
-      tmpObj.tileLocation = tmpObj.x + tmpObj.y
-
+      tmpObj.y = String.fromCharCode(64+j);
+      tmpObj.tileLocation = tmpObj.x + tmpObj.y;
+      if(i % 2 == 0){
+        if(j % 2 == 0){ tmpObj.color = "dark";}
+        else{ tmpObj.color = "light";}
+      }else{
+        if(j % 2 !== 0){ tmpObj.color = "dark";}
+        else{ tmpObj.color = "light";}
+      }
       boardArr.push(tmpObj);
     }
   }
-
 }
+
 
 (function init(){
   createBoardArr();
